bab5e803491326e124a90f4835c5e2cbd327c661
Darshan A S <itisdarshan@gmail.com>
2022-05-13 03:45:22 +0530

    Configure firebase

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,10 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'firebase_options.dart';
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const ProviderScope(child: AppWidget()));
 }
